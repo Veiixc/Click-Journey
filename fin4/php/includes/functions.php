c3c955f7073ae46139c541856f34f008a63ae3cd
ricolobangolo <168928986+ricolobangolo@users.noreply.github.com>
2025-03-16 22:32:14 +0100

    [Phase3] amélioration circuit
    [Phase3] amélioration circuit

---
+++ fin4/php/includes/functions.php
@@ -0,0 +1,34 @@
+<?php
+function readCSV($filename) {
+    $rows = array();
+    if (($handle = fopen($filename, "r")) !== FALSE) {
+        $headers = fgetcsv($handle);
+        while (($data = fgetcsv($handle)) !== FALSE) {
+            $rows[] = array_combine($headers, $data);
+        }
+        fclose($handle);
+    }
+    return $rows;
+}
+
+function writeCSV($filename, $data, $headers) {
+    $fp = fopen($filename, 'w');
+    fputcsv($fp, $headers);
+    foreach ($data as $row) {
+        fputcsv($fp, $row);
+    }
+    fclose($fp);
+}
+
+function checkAuth() {
+    session_start();
+    if (!isset($_SESSION['user_id'])) {
+        header('Location: /fin2/html/connexion.html');
+        exit();
+    }
+}
+
+function isAdmin() {
+    return isset($_SESSION['user_role']) && $_SESSION['user_role'] === 'admin';
+}
+?>
