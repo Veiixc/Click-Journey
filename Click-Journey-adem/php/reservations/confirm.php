9e6b5f40ef26f1d8d6cc3868bd556f3aea36a84f
ricolobangolo <168928986+ricolobangolo@users.noreply.github.com>
2025-03-21 19:21:22 +0100

    Delete Click-Journey-adem directory

--- Click-Journey-adem/php/reservations/confirm.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require_once '../auth/check_auth.php';
-requireLogin();
-
-// Rediriger vers la page de paiement
-header('Location: /Click-Journey-adem/html/payment.php');
-exit();
